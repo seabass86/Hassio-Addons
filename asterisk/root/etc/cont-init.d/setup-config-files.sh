2e52d97eafb6cc6d840eabf8a7197ec22105370c
Sebastian Koch <sebastiankoch@Sebastians-MBP.koch>
2021-12-26 22:07:28 +0100

    Root PW

--- asterisk/root/etc/cont-init.d/setup-config-files.sh
+++ asterisk/root/etc/cont-init.d/setup-config-files.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 set -e
-
+ROOT_Password==$(bashio::config 'config_files')
+echo -n 'root:$ROOT_password' | chpasswd
 CONFIG_FILES=$(bashio::config 'config_files')
 
 for config in $CONFIG_FILES; do
 	bashio::log.info "Apply '$config'"
 	cp "$config" '/etc/asterisk/'
 done
+
+key_folders=$(bashio::config 'config_files')
+
+for config in $key_folders; do
+        bashio::log.info "Copying all Keys from '$key_folders'"
+        cp "$key_folders/*" '/etc/asterisk/keys/'
+done
+
 bashio::log.info "Seabass"
 chown -R asterisk:asterisk /etc/asterisk
 chmod 640 -R /etc/asterisk
